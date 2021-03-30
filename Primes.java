6775a47c9051aa23fb0e76947c626a2f68415c80
SVA20 <79283121+SVA20@users.noreply.github.com>
2021-03-30 18:16:56 +0300

    Add files via upload

---
+++ Primes.java
@@ -0,0 +1,21 @@
+
+
+//данный класс выводит все простые числа от 1 до 100
+public class Primes {
+
+    public static void main(String[] args) {
+        for (int n = 2; n<101; n++)
+            if (isPrime(n))
+                System.out.print (n+" ");
+    }
+
+    //данная функция определяет является ли число простым
+    public static boolean isPrime(int n)
+    {
+        for (int i =2; i<10; i++)
+            if ((n%i==0) && (n!=i))
+                return false;
+        return true;
+    }
+
+}
